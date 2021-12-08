e44e0806dbd0717b32faa4391e070c30f97d89c1
Victor1567 <victormnh@cocodigital.co>
2021-12-08 11:34:10 -0500

    Delete answerd

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -34,10 +34,18 @@ class Answer extends Model
             // dd($answer->question);
             $answer->question->increment('answers_count');
             $answer->question->save();            
-        }); 
+        });
+        static::deleted(function ($answer){
+            $answer->question->decrement('answers_count');
+        });
     }
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
